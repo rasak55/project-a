7afaca10fd2cac3338421b8ef51635d473efa4f0
Your Name <rasak55@gmail.com>
2025-03-09 13:56:23 +0700

    create Post model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AnnouncementTypeSeeder::class);
         $this->call(AnnouncementCategorySeeder::class);
         $this->call(AnnouncementSeeder::class);
+        $this->call(PostSeeder::class);
     }
 }
